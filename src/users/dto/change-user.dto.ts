f1fdbf75152103ac49ddbaef536cbbb120439133
hellosooka <leo.zemtsoff@icloud.com>
2022-12-06 13:28:52 +0300

    add validate to user

--- src/users/dto/change-user.dto.ts
+++ src/users/dto/change-user.dto.ts
@@ -1,6 +1,15 @@
+import { IsString } from 'class-validator';
+
 export class ChangeUserDto {
+  @IsString()
   oldEmail: string;
+
+  @IsString()
   oldPassword: string;
+
+  @IsString()
   newEmail: string;
+
+  @IsString()
   newPassword: string;
 }

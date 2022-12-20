9fec6422f85ba9ed080a4df306508ada76e3cd64
hellosooka <leo.zemtsoff@icloud.com>
2022-12-20 18:06:43 +0300

    preparation to deploy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  @Get()
+  getHello() {
+    return 'Hello!';
+  }
+
   @Get('/about-us')
   getAboutUs() { }
 }
